0bf717a7ac183ebc094951e0442a68df077cf9da
fjbasile <fjbasile@icloud.com>
2020-05-14 22:33:38 -0500

    Create NeoPixelSyncedFire.ino

---
+++ NeoPixelSyncedFire.ino
@@ -0,0 +1,31 @@
+#include <Adafruit_CircuitPlayground.h>
+
+int red;
+int blue;
+int green;
+
+const int DELAY = 10;
+const int lowDel = 5;
+const int highDel = 250;
+
+const int RANDLOW = 30;
+const int RANDHI = 256;
+
+void setup()
+{
+  CircuitPlayground.begin(); 
+  randomSeed(analogRead(0));
+}
+ 
+void loop()
+{ 
+    red = random(RANDLOW, RANDHI);
+    blue = random(RANDLOW, RANDHI);
+    green = random(RANDLOW, RANDHI);
+    for (int light = 0; light < 10; light ++)
+    {
+      CircuitPlayground.setPixelColor(light, red, blue, green);
+    }
+    delay(random(lowDel,highDel)); 
+}
+ 
